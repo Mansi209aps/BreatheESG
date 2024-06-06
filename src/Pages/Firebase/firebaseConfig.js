4fa7dce337994abc005f7c2cebc040f9a9c76f6d
Mansi <mansisharma209aps@gmail.com>
2024-06-06 23:32:22 +0530

    environment variables

--- src/Pages/Firebase/firebaseConfig.js
+++ src/Pages/Firebase/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: 'AIzaSyDo4LhqdLOGZHPWZ5M9RRzz3GixxjWhMSk',
-    authDomain: 'practice-website-test-7e778.firebaseapp.com',
-    projectId: 'practice-website-test-7e778',
-    storageBucket: 'practice-website-test-7e778.appspot.com',
-    messagingSenderId: '607938130980',
-    appId: '607938130980:web:616d34b8efa93a83ded4b1',
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
 };
 
 // Initialize Firebase
